8c2d9d5bdd5fd0bde28e057077da05a5f6db4cb2
Jake Coble <jcoble@use.startmail.com>
2022-11-14 13:29:52 -0500

    Let users choose their usernames

--- public/chat.js
+++ public/chat.js
@@ -1,15 +1,21 @@
 import emoji from "node-emoji";
-import { Remarkable } from "remarkable"
-const md = new Remarkable()
+import { Remarkable } from "remarkable";
+const md = new Remarkable();
+
+let named = null;
 
 window.addEventListener("load", () => {
-  let history = []
-  const chatHistory = document.getElementById('chat-history')
-  const chatInput = document.getElementById('chat-input')
+  let history = [];
+  const body = document.body;
+  const chatHistory = document.getElementById("chat-history");
+  const chatInput = document.getElementById("chat-input");
+  const nameInput = document.getElementById("name-input");
 
-  function pushChat (message) {
-    history.push(message)
-    chatHistory.innerHTML = history.map((line) => `<li>${line}</li>`).join('\n')
+  function pushChat(message) {
+    history.push(message);
+    chatHistory.innerHTML = history
+      .map((line) => `<li>${md.render(line)}</li>`)
+      .join("\n");
   }
 
   const protocol = window.location.protocol === "https:" ? "wss" : "ws";
@@ -18,13 +24,40 @@ window.addEventListener("load", () => {
   );
 
   socket.addEventListener("message", (e) => {
-    pushChat(md.render(emoji.emojify(e.data)));
+    let message = JSON.parse(e.data);
+
+    if (message.event === "name") {
+      let prevNamed = named;
+      named = message.data;
+      body.classList.remove("waiting");
+      if (named === false) {
+        body.classList.add("naming");
+
+        if (prevNamed !== null) {
+          body.classList.add("name-rejected");
+        }
+      } else if (named === true) {
+        body.classList.remove("naming");
+        body.classList.add("chatting");
+      }
+    } else if (message.event === "chat") {
+      pushChat(emoji.emojify(message.data));
+    } else {
+      console.log(`Unrecognized event ${message.event} from server.`);
+    }
   });
 
-  chatInput.addEventListener('keypress', (event) => {
+  chatInput.addEventListener("keypress", (event) => {
     if (event.key === "Enter") {
-      socket.send(chatInput.value)
-      chatInput.value = ''
+      socket.send(chatInput.value);
+      chatInput.value = "";
     }
-  })
+  });
+
+  nameInput.addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      socket.send(nameInput.value);
+      chatInput.value = "";
+    }
+  });
 });
