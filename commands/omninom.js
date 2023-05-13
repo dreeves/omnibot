4fcc3008c78cacfb3454f5295cd0d2c2568580e5
Daniel Reeves <dreeves@beeminder.com>
2023-05-13 13:24:28 -0700

    Voxmode testing

--- commands/omninom.js
+++ commands/omninom.js
@@ -5,18 +5,23 @@ const packageData = require('../package.json');
 module.exports = {
 
 name: NOM,
-description: "An Omnibot test slash command for experimenting with",
+description: "An Omnibot slash command for experimenting with",
 input: {
   name: "arg1",
   required: true,
   description: "This is the usage hint: type something",
 },
 execute: ({ channel_name, sender, input }) => {
- return { voxmode: "whisp", output: `\
+  let voxmode = "whisp";
+  if (input === "holla" || 
+      input === "blurt") { voxmode = input }
+  return { voxmode, output: `\
 This is Omnibot v${packageData.version} \
 called by ${sender} \
 in channel ${channel_name}.\n\
 You called /${NOM} with arg1 = "\`${input}\`".\n\
+For testing, you can make arg1 be "whisp", "holla", or "blurt" and /omninom \
+will use that as the voxmode for this reply.\n\
 Debugging factoid: ` + (input === input.trim() ? 
 "If arg1 had leading or trailing whitespace, it got trimmed before Omnibot " +
 "saw it." :
