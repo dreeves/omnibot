b6aa156d31b061375540f8e82875e6ae6c4d1cc6
Jake Coble <jcoble@use.startmail.com>
2023-06-07 14:40:45 -0400

    Move event handling for platforms into submodule

--- platforms/discord.js
+++ platforms/discord.js
@@ -1,5 +1,7 @@
 const Discord = require("discord.js");
 
+const { lexup } = require("../lexiguess.js");
+
 const discord = new Discord.Client({
     intents: [
         Discord.GatewayIntentBits.Guilds,
@@ -9,5 +11,46 @@ const discord = new Discord.Client({
 });
 
 discord.commands = new Discord.Collection();
+
+discord.once("ready", () => {
+    console.log(
+        `Omnibot is running; logged in to Discord as ${discord.user.tag}`
+    );
+});
+discord.on("interactionCreate", async (interaction) => {
+    if (!interaction.isChatInputCommand()) return;
+
+    const command = interaction.client.commands.get(interaction.commandName);
+
+    if (!command) {
+        console.error(
+            `No command matching ${interaction.commandName} was found.`
+        );
+        return;
+    }
+
+    try {
+        await command.execute(interaction);
+    } catch (error) {
+        console.error(error);
+        await interaction.reply({
+            content: "There was an error while executing this command!",
+            ephemeral: true,
+        });
+    }
+});
+
+discord.on("messageCreate", async (msg) => {
+    if (msg.author.bot) {
+        return;
+    }
+
+    const cid = msg.channel.name; // string identifier for this server/channel
+    const usaid = msg.content;
+    if (!/^[a-z]{2,}$/i.test(usaid)) return; // DRY up this regex
+    if (!/^(?:botspam|games|lexi.*|spellingbee)$/.test(cid)) return;
+    const reply = lexup(cid, usaid);
+    if (reply !== null) await msg.reply(reply);
+});
 
 module.exports = discord;
--- platforms/slack.js
+++ platforms/slack.js
@@ -1,4 +1,7 @@
 const { App, ExpressReceiver } = require("@slack/bolt");
+
+const { lexup } = require("../lexiguess.js");
+
 const receiver = new ExpressReceiver({
     signingSecret: process.env.SLACK_SIGNING_SECRET,
 });
@@ -14,6 +17,59 @@ const appOptions = process.env.DEBUG
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
     ...appOptions,
+});
+
+// Someone says a single strictly alphabetic word in a channel our bot is in
+app.message(/^\s*([a-z]{2,})\s*$/i, async ({ context, say }) => {
+    // DRY me
+    const cid = context.teamId; // string identifier for this server/channel
+    const usaid = context.matches[0]; // the string the user typed
+    const reply = lexup(cid, usaid);
+
+    if (reply !== null) await say(reply);
+});
+
+// Someone clicks on the Home tab of our app in Slack; render the page
+app.event("app_home_opened", async ({ event, context }) => {
+    try {
+        console.log(`Home tab opened in Slack by user ${event.user}`);
+        await app.client.views.publish({
+            token: context.botToken,
+            user_id: event.user, // user who opened our app's app home tab
+            view: {
+                // the view payload that appears in the app home tab
+                type: "home",
+                callback_id: "home_view",
+                blocks: [
+                    // body of the view
+                    {
+                        type: "section",
+                        text: {
+                            type: "mrkdwn",
+                            text: "Welcome to Omnibot :books:",
+                        },
+                    },
+                    { type: "divider" },
+                    {
+                        type: "section",
+                        text: {
+                            type: "mrkdwn",
+                            text: `\
+Instructions for Lexiguess (the main thing Omnibot does so far): \
+The bot totally ignores anything that isn't a single word \
+(at least 2 letters, no punctuation). \
+That's really all you need to know. \
+Everything else should be self-explanatory.\
+
+There's also a /bid command for sealed-bid auctions.`,
+                        },
+                    },
+                ],
+            },
+        });
+    } catch (error) {
+        console.error(error);
+    }
 });
 
 module.exports = app;
