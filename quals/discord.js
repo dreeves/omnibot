2ae3da229cef8fb921d0a8f0f317dcd7dccc5927
Jake Coble <jcoble@use.startmail.com>
2023-07-19 14:50:49 -0400

    Write Discord tests for non-ephemeral messages

--- quals/discord.js
+++ quals/discord.js
@@ -1,14 +1,87 @@
+const sinon = require("sinon");
+const chai = require("chai");
+const expect = chai.expect;
+const chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+
 const sendmesg = require("../platforms/discord/sendmesg");
 
 describe("sending a message to Discord", function () {
+    const interactionCache = {};
+
+    const fakeUser = { send: sinon.fake.resolves() };
+    const fakeMessage = { reply: sinon.fake() };
+    const fakeCollection = (item) => ({
+        fetch: (id) => (id ? Promise.resolve(item) : Promise.resolve([item])),
+    });
+    const fakeChannel = {
+        name: "botspam",
+        send: sinon.fake.resolves(),
+        messages: fakeCollection(fakeMessage),
+    };
+    const fakeGuild = {
+        name: "testserver",
+        fetch: () => Promise.resolve(fakeGuild),
+        channels: fakeCollection(fakeChannel),
+    };
+
+    const fakeClient = {
+        guilds: fakeCollection(fakeGuild),
+        users: fakeCollection(fakeUser),
+    };
+
     describe("sending a non-ephemeral message", function () {
-        xit("replies to a message if mrid and mesg are present", function () {});
+        it("replies to a message if fief, chan, mrid, and mesg are present", async function () {
+            const message = {
+                plat: "discord",
+                fief: "testserver",
+                chan: "botspam",
+                mrid: "123",
+                mesg: "Hello, world!",
+            };
 
-        xit("sends a DM if priv, user, and message are present", function () {});
+            await sendmesg(fakeClient, interactionCache, message);
+            expect(fakeMessage.reply.calledWith(message.mesg)).to.be.true;
+        });
 
-        xit("sends a message in a channel if fief, chan, and message are present", function () {});
+        it("sends a DM if priv, user, and mesg are present", async function () {
+            const message = {
+                plat: "discord",
+                user: "<@123>",
+                priv: true,
+                mesg: "Hello, world!",
+            };
 
-        xit("throws an error if anything else is true", function () {});
+            await sendmesg(fakeClient, interactionCache, message);
+            expect(fakeUser.send.calledWith(message.mesg)).to.be.true;
+        });
+
+        it("sends a message in a channel if fief, chan, and mesg are present", async function () {
+            const message = {
+                plat: "discord",
+                fief: "testserver",
+                chan: "botspam",
+                mesg: "Hello, world!",
+            };
+
+            await sendmesg(fakeClient, interactionCache, message);
+            expect(fakeChannel.send.calledWith(message.mesg)).to.be.true;
+        });
+
+        it("throws an error if anything else is true", async function () {
+            const message = {
+                plat: "discord",
+                user: "<@123>",
+                priv: true,
+                mesg: "Hello, world!",
+                fief: "testserver",
+                chan: "botspam",
+                mesg: "Hello, world!",
+            };
+
+            expect(sendmesg(fakeClient, interactionCache, message)).to.be
+                .rejected;
+        });
     });
 
     describe("sending an ephemeral message", function () {
