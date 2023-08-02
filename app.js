d7e5a0f686a90b35e238b65ac4636dfd88373361
Jake Coble <jcoble@use.startmail.com>
2023-08-02 17:36:12 -0400

    Log uncaught exceptions

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+process.on('uncaughtException', function (exception) {
+  console.log(exception); // to see your exception details in the console
+  // if you are on production, maybe you can send the exception details to your
+  // email as well ?
+});
+
 "use strict";
 const CLOG = console.log;
 CLOG("Omnibot!");
