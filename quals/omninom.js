028899b8c5f7abe2def573f559fc54a33f81a4c6
Jake Coble <jcoble@use.startmail.com>
2023-09-14 12:00:36 -0400

    Refactor /omninom tests and add integration tests

--- quals/omninom.js
+++ quals/omninom.js
@@ -4,46 +4,162 @@ const expect = chai.expect;
 const chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 
+const DiscordFake = require("./fakes/discord.js");
+const SlackFake = require("./fakes/slack.js");
 const omninom = require("../commands/omninom.js");
+const { sendmesg, registerPlatform } = require("../sendemitter.js");
+
+const discordSendmesg = require("../platforms/discord/sendmesg.js");
+const slackSendmesg = require("../platforms/slack/sendmesg.js");
 
 const packageData = require("../package.json"); // to see the version number
 
-describe('running the command with "foo" as an argument', function () {
-    it("calls sendmesg with a whisper", async function () {
-        const sendmesg = sinon.fake.returns(Promise.resolve());
-        let message = {
+const discordExpectations = [
+    {
+        desc: "replies to whisp via DM (channel message)",
+        input: {
             plat: "discord",
-            fief: "dummy",
-            chan: "#botspam",
+            fief: "testserver",
+            chan: "botspam",
             user: "<@1234>",
-            mesg: "foo",
-            msid: "1234",
-        };
-        await omninom(sendmesg, message);
-        sinon.assert.calledWith(sendmesg, {
-            plat: message.plat,
-            mesg: sinon.match(/.*/),
-            mrid: message.msid,
-            phem: true,
-        });
+            mesg: "whisp",
+            msid: "interaction:1234",
+        },
+        output: [
+            {
+                plat: "discord",
+                mesg: sinon.match.string,
+                phem: true,
+                mrid: "interaction:1234",
+            },
+            {
+                plat: "discord",
+                priv: true,
+                user: "<@1234>",
+                mesg: sinon.match.string,
+            },
+        ],
+    },
+    {
+        desc: "replies to whisp via DM (DM message)",
+        input: {
+            plat: "discord",
+            priv: true,
+            user: "<@1234>",
+            mesg: "whisp",
+            msid: "interaction:1234",
+        },
+        output: [
+            {
+                plat: "discord",
+                mesg: sinon.match.string,
+                user: "<@1234>",
+                priv: true,
+                mrid: "interaction:1234",
+            },
+        ],
+    },
+];
+
+const slackExpectations = [
+    {
+        desc: "replies to whisp via DM (channel message)",
+        input: {
+            plat: "slack",
+            fief: "testserver",
+            chan: "botspam",
+            user: "<@1234>",
+            mesg: "whisp",
+            msid: "interaction:1234",
+        },
+        output: [
+            {
+                plat: "slack",
+                mesg: sinon.match.string,
+                phem: true,
+                mrid: "interaction:1234",
+            },
+            {
+                plat: "slack",
+                priv: true,
+                user: "<@1234>",
+                mesg: sinon.match.string,
+            },
+        ],
+    },
+    {
+        desc: "replies to whisp via DM (DM message)",
+        input: {
+            plat: "slack",
+            priv: true,
+            user: "<@1234>",
+            mesg: "whisp",
+            msid: "interaction:1234",
+        },
+        output: [
+            {
+                plat: "slack",
+                mesg: sinon.match.string,
+                user: "<@1234>",
+                priv: true,
+                mrid: "interaction:1234",
+            },
+        ],
+    },
+];
+
+describe("running /omninom on Discord", function () {
+    let interactionCache = {};
+
+    before(async function () {
+        registerPlatform("discord", (message) =>
+            discordSendmesg(DiscordFake, interactionCache, message),
+        );
     });
 
-    it("responds to a Slack DM with a DM", async function () {
-        const sendmesg = sinon.fake.returns(Promise.resolve());
-        let message = {
-            plat: "slack",
-            user: "<@1234>",
-            mesg: "holla",
-            msid: "1234",
-            priv: true,
-        };
-        await omninom(sendmesg, message);
-        sinon.assert.calledWith(sendmesg, {
-            plat: message.plat,
-            mrid: message.msid,
-            user: message.user,
-            mesg: sinon.match.string,
-            priv: true,
+    afterEach(async function () {
+        sinon.restore();
+    });
+
+    discordExpectations.forEach((exp) => {
+        it(exp.desc, async function () {
+            const sendmesg = sinon.fake.resolves();
+            await omninom(sendmesg, exp.input);
+
+            exp.output.forEach((out) => sinon.assert.calledWith(sendmesg, out));
+        });
+
+        it(`${exp.desc} without error`, async function () {
+            const promise = omninom(sendmesg, exp.input);
+            return expect(promise).to.be.fulfilled;
+        });
+    });
+});
+
+describe("running /omninom on Slack", function () {
+    let interactionCache = {};
+
+    before(async function () {
+        registerPlatform("slack", (message) =>
+            slackSendmesg(SlackFake, interactionCache, message),
+        );
+    });
+
+    afterEach(async function () {
+        sinon.restore();
+    });
+
+    slackExpectations.forEach((exp) => {
+        it(exp.desc, async function () {
+            const sendmesg = sinon.fake.resolves();
+            await omninom(sendmesg, exp.input);
+
+            exp.output.forEach((out) => sinon.assert.calledWith(sendmesg, out));
+        });
+
+        it(`${exp.desc} without error`, async function () {
+            const promise = omninom(sendmesg, exp.input);
+            return expect(promise).to.be.fulfilled;
         });
     });
 });
